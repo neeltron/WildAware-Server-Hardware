a17e8ee24e6e6fec61b646f3b5c710687641af9d
Neel Adwani <nprosofficial@gmail.com>
2021-10-02 20:18:24 +0000

    connected db to entrypoint

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from replit import db
+import random
+
 app = Flask('app')
 
 @app.route('/')
@@ -7,10 +10,12 @@ def hello_world():
 
 @app.route('/input', methods = ['GET'])
 def input():
+  temp = random.randint(0, 1000)
   aname = request.args.get('aname')
   loc = request.args.get('loc')
   desc = request.args.get('desc')
   info = [aname, loc, desc]
+  db[str(temp)] = info
   return jsonify(info)
 
 app.run(host='0.0.0.0', port=8080)
