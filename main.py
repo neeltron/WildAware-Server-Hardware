e94dc56e710d3e2dca9880de738ed279a7c8d102
Neel Adwani <nprosofficial@gmail.com>
2021-10-02 20:15:31 +0000

    added input point

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/input', methods = ['GET'])
+def input():
+  aname = request.args.get('aname')
+  loc = request.args.get('loc')
+  desc = request.args.get('desc')
+  info = [aname, loc, desc]
+  return jsonify(info)
 
 app.run(host='0.0.0.0', port=8080)
