a66da6c9ae2ab4874a21229b2d9111770e26850c
Neel Adwani <nprosofficial@gmail.com>
2021-10-03 09:20:58 +0000

    added function to split string and receive data in nodemcu

---
+++ hardware/hardware.ino
@@ -0,0 +1,25 @@
+String incomingByte;
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.readString();
+    String name = getValue(incomingByte, ' ', 0);
+    Serial.println(name);
+  }
+}
